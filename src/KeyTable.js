128ccf352e65ad53db630629913ca1fe78ceb646
alexandersantosduvall@gmail.com <alexandersantosduvall@gmail.com>
2021-02-24 22:47:43 -0600

    Add textbox input to KeyTable.js. When second column changes, the third will accordingly. Bug: Third column doesn't change.

--- src/KeyTable.js
+++ src/KeyTable.js
@@ -4,40 +4,94 @@ class KeyTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tableElements: "",
-            tableValues: [["host1", "host"], ["Router1", "router"], ["Bridge2", "Bridge"], ["Gateway", "Gateway"]]
+            tableValues: []
         }
+        this.removeObjFromArray = this.removeObjFromArray.bind(this);
+        this.addRow = this.addRow.bind(this);
+        this.constructOptions = this.constructOptions.bind(this);
+        this.changeOption = this.changeOption.bind(this);
+        this.constructDropdown = this.constructDropdown.bind(this);
     }
+
+    removeObjFromArray(index) {
+        let variables = this.state.tableValues;
+        variables.splice(index, 1)
+        this.constructTable();
+    }
+
+    addRow() {
+        let variables = this.state.tableValues;
+        variables.push(["Host"])
+        this.constructTable();
+    }
+
+    changeOption(e, index) {
+        let v = this.state.tableValues;
+        console.log("asdfasfasdf");
+        console.log(e);
+        v[index] = e.target.value.toString();
+        this.setState({
+            tableValues: v
+        })
+        this.constructDropdown(this.state.tableValues[index])
+    }
+
+    constructOptions(i) {
+        return <select onChange={(e) => this.changeOption(e, i)} className={"headerSelector"}>
+            <option selected value={"host"}>Host</option>
+            <option value={"vlan"}>Vlan</option>
+            <option value={"gateway"}>Gateway</option>
+            <option value={"port"}>Port</option>
+        </select>
+    }
+
+    constructDropdown(param) {
+        console.log("constructing with: " + param +"_");
+        switch (param) {
+            case "host":
+                console.log("host.....")
+                return <select className={"headerSelector"}>
+                    <option>Host stuff</option>
+                </select>
+            case "bridge":
+                console.log("bridge.....")
+                return <select className={"headerSelector"}>
+                    <option>Bridge stuff</option>
+                </select>
+            case "router":
+                console.log("router.....")
+                return <select className={"headerSelector"}>
+                    <option>Router stuff</option>
+                </select>
+            case "gateway":
+                console.log("gateway.....")
+                return <select className={"headerSelector"}>
+                    <option>Gateway stuff</option>
+                </select>
+            default:
+                console.log(".....default")
+                return <select className={"headerSelector"}>
+                    <option>default stuff</option>
+                </select>
+        }
+    };
 
     constructTable() {
         let variables = this.state.tableValues;
-        let constructDropdown = (param) => {
-            switch (param.toLowerCase()) {
-                case "host":
-                    return <select className={"headerSelector"}>
-                        <option>Host stuff</option>
-                    </select>
 
-                case "bridge":
-                    return <select className={"headerSelector"}>
-                        <option>Bridge stuff</option>
-                    </select>
-                case "router":
-                    return <select className={"headerSelector"}>
-                        <option>Router stuff</option>
-                    </select>
-                case "gateway":
-                    return <select className={"headerSelector"}>
-                        <option>Gateway stuff</option>
-                    </select>
-            }
-        };
+
         let a = [];
 
         for (let i = 0; i < variables.length; i++) {
             a.push(<tr key={i}>
-                <td className={"tdFormat"} key={i + "child_1"}>{variables[i][0]}</td>
-                <td key={i + "child_2"}>{constructDropdown(variables[i][1])}</td>
+                <td className={"tdButton"}>
+                    <button className="Append" onClick={() => this.removeObjFromArray(i)}>
+                        -
+                    </button>
+                </td>
+                <td className={"tdFormat"} key={i + "child_1"}><input placeholder="Give variable a name"/></td>
+                <td key={i + "child_2"}>{this.constructOptions(i)}</td>
+                <td key={i + "child_3"}>{this.constructDropdown(this.state.tableValues[i])}</td>
             </tr>);
             console.log("going through matrix:");
         }
@@ -53,13 +107,20 @@ class KeyTable extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className={"keyTable"}>
-                    <table className="tableSpacing">
-                        <h3>Key Table</h3>
-                        <tbody>
-                        {this.state.tablesElements}
-                        </tbody>
-                    </table>
+                <div className="keyTable">
+                    <div className="imageToggler">
+                        <button className="Append appendKey" onClick={() => this.addRow()}>
+                            +
+                        </button>
+                        <div>
+                            <h3 id="KeyTableLabel">Key Table</h3>
+                            <table className="tableSpacing">
+                                <tbody>
+                                {this.state.tablesElements}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         )
